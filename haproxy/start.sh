00b5fe70537d7026d5fc9eda4e1e0aafaa69be57
luismiguel.saez <luismiguelsaez83@gmail.com>
2021-11-22 21:20:12 +0100

    Fixed sed command

--- haproxy/start.sh
+++ haproxy/start.sh
@@ -12,15 +12,17 @@ while true
 do
   if [ $(( $c % 2 )) -eq 0 ]
   then
-    sed -i -e 's/\( server web-0\)/#\1/g' -e 's/# \(server web-1\)/ \1/g' /tmp/haproxy.conf
+    echo Is pair
+    sed -i -e 's/    \(server web-0\)/    #\1/g' -e 's/    #\(server web-1\)/    \1/g' /tmp/haproxy.conf
   else
-    sed -i -e 's/\( server web-1\)/#\1/g' -e 's/# \(server web-0\)/ \1/g' /tmp/haproxy.conf
+    echo Is not pair
+    sed -i -e 's/    \(server web-1\)/    #\1/g' -e 's/    #\(server web-0\)/    \1/g' /tmp/haproxy.conf
   fi
 
   sudo /usr/local/sbin/haproxy -D -f /tmp/haproxy.conf -p /tmp/haproxy.pid -sf $(cat /tmp/haproxy.pid) 2>&1 > /tmp/haproxy.log
 
   c=$(( $c + 1 ))
-  sleep 0.5
+  sleep 2
 done
 
 while true
